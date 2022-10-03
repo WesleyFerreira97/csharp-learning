307d2041afc8bc59c1091e69b223f8a6d6c8ae8b
Wesley Ferreira <wesleyfc00@hotmail.com>
2022-10-03 04:06:13 -0300

    added example C# Props

---
+++ BasicExamples/Examples/ExProps.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// https://www.tutorialspoint.com/csharp/csharp_variables.htm
+// https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/properties
+
+namespace BasicExamples.Examples
+{
+    public class Character
+    {
+        private string name = "No Name";
+        
+        public string Name 
+        {
+            get => name;
+            set {
+                name = value;
+            }
+            
+        }
+
+    }
+}
--- BasicExamples/Program.cs
+++ BasicExamples/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using BasicExamples.Examples;
+
+
+
+Character RickSanchez = new Character();
+
+RickSanchez.Name = "Rick Sanchez";
+
+Console.WriteLine(RickSanchez.Name);
