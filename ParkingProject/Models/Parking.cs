96f86c3770e6dc53ded9cc89852882b2644c9617
Wesley Ferreira <wesleyfc00@hotmail.com>
2022-10-05 17:59:16 -0300

    Init Parking Project

---
+++ ParkingProject/Models/Parking.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace estacionamento.Models
+{
+    internal class Parking
+    {
+        private decimal _initialPrice;
+        private decimal _pricePerHour;
+
+        public Parking(decimal initialPrice, decimal pricePerHour)
+        {
+            InitialPrice = initialPrice;
+            PricePerHour = pricePerHour;
+        }
+
+        public decimal InitialPrice { 
+            get => _initialPrice; 
+            set { 
+                _initialPrice = value; 
+            } 
+        }
+        public decimal PricePerHour { 
+            get => _initialPrice; 
+            set { 
+                _initialPrice = value; 
+            } 
+        }
+    }
+}
---
+++ ParkingProject/Program.cs
@@ -0,0 +1,18 @@
+﻿using estacionamento.Models;
+
+decimal initialPrice = 0;
+decimal pricePerHour = 0;
+
+Console.WriteLine("Bem Vindo ao sistema de estacionamento!");
+Console.WriteLine("Digite o preço inicial : ");
+
+initialPrice = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine("Digite o preço por hora : ");
+
+pricePerHour = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine($"Preço inicial definido em : {initialPrice}");
+Console.WriteLine($"Preço por hora definido em : {pricePerHour}");
+
+Parking theParking = new Parking(initialPrice, pricePerHour);
