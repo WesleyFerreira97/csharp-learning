b673ccd6b70500618f57860eab5718519f8d4da8
Wesley Ferreira <wesleyfc00@hotmail.com>
2022-10-05 16:58:32 -0300

    Added serialize to props

--- BasicExamples/Examples/ExDeserialize.cs
+++ BasicExamples/Examples/ExDeserialize.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace BasicExamples.Examples
 {
     internal class ExDeserialize
     {
         public int Id { get; set; }
+        
+        // Converte uma possivel entrada fora do formato para outro identificador
+        // na convenção de nomeclatura do C#
+        [JsonProperty("Nome_Produto")]
         public string Product { get; set; }
         public decimal Price { get; set; }
         public DateTime DateSale { get; set; }
